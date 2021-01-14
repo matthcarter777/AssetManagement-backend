d9d63db39bd136116620bce4d748b5c07b6daa6f
mateus <mateushbsb@gmail.com>
2021-01-14 09:06:39 -0300

    all

---
+++ routes/index.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express'; 
+
+
+const routes = Router();
+
+
+routes.get('/', (req, res) => {
+    res.status(200).send('Welcome to Assets Managements');
+});
+
+export default routes;
