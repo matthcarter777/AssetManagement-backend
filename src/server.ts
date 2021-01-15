3a279ad85b66ace71d627130067b540dd353365e
Mateus <mateushbsb@gmail.com>
2021-01-15 13:55:35 -0300

    add typeorm and migration

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 
-import routes from '../routes/index';
+import routes from './routes/index';
+import './database';
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 app.use(routes);
 
 app.listen(3333, () => {
