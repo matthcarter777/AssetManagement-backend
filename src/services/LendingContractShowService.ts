237d5d4b40356aa56d287239f6881d4a5bdd5274
Mateus <mateushbsb@gmail.com>
2021-03-13 20:17:24 -0300

    LendingContractShowService updated

--- src/services/LendingContractShowService.ts
+++ src/services/LendingContractShowService.ts
@@ -2,17 +2,31 @@ import { AppError } from './../Errors/AppError';
 import { getCustomRepository } from 'typeorm';
 
 import LendingContractRepository from '../repositories/LendingContractRepository';
+import EquipmentRepository from '../repositories/EquipmentsRepository';
+import UserRepository from '../repositories/UserRepository';
 
 class LendingContractShowService {
   async execute(id: string) {
 
     const lendingContractRepository = getCustomRepository(LendingContractRepository);
+    const userRepository = getCustomRepository(UserRepository);
+    const equipmentRepository = getCustomRepository(EquipmentRepository);
 
-    const lendingContract = await lendingContractRepository.findById(id);
+    const findLendingContract = await lendingContractRepository.findById(id);
+    const users = await userRepository.find();
+    const equipment = await equipmentRepository.find();
 
-    if(!lendingContract) {
+    if(!findLendingContract) {
       throw new AppError('Lending contract not already exist!');
     };
+
+    const lendingContract = {
+      id: findLendingContract.id,
+      name: users.find(user => user.id === findLendingContract.user_id).name,  
+      cpf: users.find(user => user.id === findLendingContract.user_id).cpf,
+      equipment: equipment.find(equipment => equipment.id === findLendingContract.equipment_id).description,
+      identification: equipment.find(equipment => equipment.id === findLendingContract.equipment_id).identification,
+    }
 
     return lendingContract;
   }
