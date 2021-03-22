e3223df2271f424bc0a3936c2a50a0a31dc21004
Mateus <mateushbsb@gmail.com>
2021-03-22 19:48:09 -0300

    dashboard

--- src/services/DashboardService.ts
+++ src/services/DashboardService.ts
@@ -15,31 +15,11 @@ class DashboardIndexService {
       return {
         name: type.name,
         total: equipmentsFind.filter(equipment => equipment.type_id === type.id).length,
-        available: equipmentsFind.filter(equipment => equipment.type_id === type.id && equipment.isAvailable === true ).length,
+        totalAvailable: equipmentsFind.filter(equipment => equipment.type_id === type.id && equipment.isAvailable === true ).length,
       }
     })
-
-    console.log(typesFiltered)
-
-    /* Provisory search */
-    const notebookId = typesFind.find(type => type.name === 'Notebook').id;
-    const cellphoneId = typesFind.find(type => type.name === 'Celular').id;
-    const radioId = typesFind.find(type => type.name === 'Radio').id;
-    const chipId = typesFind.find(type => type.name === 'Chip').id;
-
-
-    const equipments = {
-      radioTotal: equipmentsFind.filter(equipment => equipment.type_id === radioId).length,
-      radioAvailable: equipmentsFind.filter(equipment => equipment.type_id === radioId && equipment.isAvailable === true ).length,
-      notebookTotal: equipmentsFind.filter(equipment => equipment.type_id === notebookId ).length,
-      notebookAvailable: equipmentsFind.filter(equipment => equipment.type_id === notebookId && equipment.isAvailable === true).length,
-      cellphoneTotal: equipmentsFind.filter(equipment => equipment.type_id === cellphoneId).length,
-      cellphoneAvailable: equipmentsFind.filter(equipment => equipment.type_id === cellphoneId && equipment.isAvailable === true).length,
-      simTotal: equipmentsFind.filter(equipment => equipment.type_id === chipId).length,
-      simAvailable: equipmentsFind.filter(equipment => equipment.type_id === chipId && equipment.isAvailable === true).length,
-    }
     
-    return equipments;
+    return typesFiltered;
   }
 
 }
